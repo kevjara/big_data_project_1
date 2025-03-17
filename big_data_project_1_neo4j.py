2a49170cd1d70ae98577b932dbbe9c42ea3f842d
Jeffrey Oyuela <jeffreysmac@Jeffreys-MacBook-Pro.local>
2025-03-17 04:30:37 -0400

    Added both queries to program

--- big_data_project_1_neo4j.py
+++ big_data_project_1_neo4j.py
@@ -7,6 +7,11 @@ Given a disease ID:
     * What are gene names that cause this disease?
     * Where does this disease occur?
 '''
+def createDatabase(driver, database_name):
+    driver.execute_query(f"CREATE DATABASE {database_name}")
+
+
+
 def createDatabaseNodes(session):
     
     def createIndexQuery(label): 
@@ -34,10 +39,10 @@ def createDatabaseNodes(session):
         CALL (row, source, target) {
             FOREACH (_ IN CASE WHEN row.metaedge IN ['CrC', 'DrD'] THEN [1] ELSE [] END | CREATE (source)-[:RESEMBLES]->(target))
             FOREACH (_ IN CASE WHEN row.metaedge = 'CtD' THEN [1] ELSE [] END | CREATE (source)-[:TREATS]->(target))
-            FOREACH (_ IN CASE WHEN row.metaedge = 'CpC' THEN [1] ELSE [] END | CREATE (source)-[:PALLIATES]->(target))
-            FOREACH (_ IN CASE WHEN row.metaedge IN ['CuG', 'DuA', 'AuG', 'DuG'] THEN [1] ELSE [] END | CREATE (source)-[:UPREGULATES]->(target))
+            FOREACH (_ IN CASE WHEN row.metaedge = 'CpD' THEN [1] ELSE [] END | CREATE (source)-[:PALLIATES]->(target))
+            FOREACH (_ IN CASE WHEN row.metaedge IN ['CuG', 'AuG', 'DuG'] THEN [1] ELSE [] END | CREATE (source)-[:UPREGULATES]->(target))
             FOREACH (_ IN CASE WHEN row.metaedge = 'CbC' THEN [1] ELSE [] END | CREATE (source)-[:BINDS]->(target))
-            FOREACH (_ IN CASE WHEN row.metaedge IN ['CdC', 'DdA', 'AdG', 'DdG'] THEN [1] ELSE [] END | CREATE (source)-[:DOWNREGULATES]->(target))
+            FOREACH (_ IN CASE WHEN row.metaedge IN ['CdC', 'AdG', 'DdG'] THEN [1] ELSE [] END | CREATE (source)-[:DOWNREGULATES]->(target))
             FOREACH (_ IN CASE WHEN row.metaedge = 'DlA' THEN [1] ELSE [] END | CREATE (source)-[:LOCALIZES]->(target))
             FOREACH (_ IN CASE WHEN row.metaedge = 'DaG' THEN [1] ELSE [] END | CREATE (source)-[:ASSOCIATES]->(target))
             FOREACH (_ IN CASE WHEN row.metaedge = 'AeG' THEN [1] ELSE [] END | CREATE (source)-[:EXPRESSES]->(target))
@@ -53,26 +58,26 @@ def createDatabaseNodes(session):
     session.run(nodesQuery)
     session.run(relationshipsQuery)
 
-def getDiseaseQuery(diseaseId):
+def getDiseaseQuery(session, diseaseId):
     query = """
         MATCH (d:Disease {id: '""" + diseaseId + """'})
-        OPTIONAL MATCH (c:Compound)-[:TREATS]->(d)
-        OPTIONAL MATCH (c:Compound)-[:PALLIATES]->(d)
+        OPTIONAL MATCH (c:Compound)-[:TREATS|PALLIATES]->(d)
         OPTIONAL MATCH (d)-[:ASSOCIATES]->(g:Gene)
         OPTIONAL MATCH (d)-[LOCALIZES]->(a:Anatomy)
-        RETURN d.name, collect(c.name) AS drug_names, collect(g.name) AS gene_names, collect(a.name) AS disease_locations
+        RETURN d.name, collect(DISTINCT c.name) AS drug_names, collect(DISTINCT g.name) AS gene_names, collect(DISTINCT a.name) AS disease_locations
     """
-    return query
+
+    return session.run(query)
 
 driver = GraphDatabase.driver('bolt://localhost:7687', auth=('neo4j', 'bigdatatechnology'))
 session = driver.session()
 
-createDatabaseNodes(session)
+#createDatabaseNodes(session)
 
 diseaseId = input("Enter Disease ID: ")
-print (session.run(getDiseaseQuery(diseaseId)))
-
-session.close()
+diseaseDetails = [record['d.name'] for record in getDiseaseQuery(session, diseaseId)]
+for i in diseaseDetails:
+    print(i)
 
 '''
 Find all compounds that can treat a NEW DISEASE
@@ -80,3 +85,25 @@ A compound can treat a new disease if:
     * The compound up-regulates/down-regulates a gene
     * The location of the disease down-regulates/up-regulates the gene respectively
 '''
+
+def findCompoundForDisease(session, diseaseId):
+    query = """
+        MATCH (d:Disease {id: '""" + diseaseId + """'})
+        MATCH (c:Compound)-[r1:UPREGULATES|DOWNREGULATES]->(g:Gene)
+        MATCH (d)-[:LOCALIZES]->(a:Anatomy)
+        MATCH (a)-[r2:DOWNREGULATES|UPREGULATES]->(g)
+        WHERE
+            ((type(r1) = 'UPREGULATES' AND type(r2) = 'DOWNREGULATES') OR
+            (type(r1) = 'DOWNREGULATES' AND type(r2) = 'UPREGULATES')) AND
+            NOT EXISTS { (c)-[:TREATS]->(d) }
+        RETURN DISTINCT c.name;
+    """
+
+    return session.run(query)
+
+diseaseId = input("Enter Disease Id: ")
+potentialCompounds = [record["c.name"] for record in findCompoundForDisease(session, diseaseId)]
+for i in potentialCompounds:
+    print(i)
+session.close()
+driver.close()
